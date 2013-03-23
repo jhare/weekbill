cc6d49d9592cb9b5e65b26e480f1abbc3912dc67
Joe Hare <joseph.hare@gmail.com>
2013-03-22 21:36:49 -0500

    Library and css stuff I need

---
+++ public/javascripts/blah.js
@@ -0,0 +1,3 @@
+define({
+    baz: "joe"
+});
---
+++ public/javascripts/main.js
@@ -0,0 +1,7 @@
+
+require(["helper/util","blah"], function(util, blah) {
+   console.log("Util has " + util.foo);
+
+   console.log("Blah has " + blah.baz);
+
+});
