c5d35007b983cf194434df122b53453458565297
jhare <joseph.hare@gmail.com>
2013-03-25 22:48:53 -0500

    Integrated a social bar, expanded views for testing.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,15 @@
-define(['jquery'], function() {
+define(['jquery', 'bootstrap' ], function($, bootstrap) {
 
   console.log("Loaded main!");
+
+  var copy = $(".day-row").clone();
+  
+  for( var i = 0; i < 50; i++ ) {
+    $('.bill-flow-container').append( copy.clone() );
+  }
+
+  $('.social-bar').affix({
+    offset: 10
+  });
+
 });
