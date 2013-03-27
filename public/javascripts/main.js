6f4b737711480355e7c44feb03745a8bc4d23896
jhare <joseph.hare@gmail.com>
2013-03-26 21:13:30 -0500

    Removing test output for social bar.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,12 +1,3 @@
 define(['jquery', 'bootstrap' ], function($, bootstrap) {
-
-  console.log("Loaded main!");
-
-  var copy = $(".day-row").clone();
-  
-  for( var i = 0; i < 50; i++ ) {
-    $('.bill-flow-container').append( copy.clone() );
-  }
-
   $('.social-bar').affix();
 });
